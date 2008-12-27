12b5a77748717414607a5d289adf9f8eeeed89c1
Konstantin Merenkov <kmerenkov@gmail.com>
2008-12-27 04:09:18 +0300

    Manager can guess whether you want to upgrade or downgrade and build the correct patching trace.

--- manager.py
+++ manager.py
@@ -4,10 +4,20 @@ import version_catalogs
 
 class Manager(object):
     def __init__(self):
-        self.provider = version_providers.DummyVersionProvider('1.0')
+        self.provider = version_providers.DummyVersionProvider('3.0')
         self.catalog = version_catalogs.DummyVersionsCatalog(['1.0', '2.0', '3.0', '4.0', '5.0'])
 
     def build_patching_trace(self, to=None):
+        def is_upgrade(ver_from, ver_to):
+            if not ver_to:
+                return True
+            x = [ver_from, ver_to]
+            x.sort()
+            if x[0] == ver_from:
+                return True
+            return False
+
+        # get current version and all available versions
         cur_ver = self.provider.get_current_version()
         all_vers = self.catalog.get_available_versions()
         all_vers.sort()
@@ -15,10 +25,17 @@ class Manager(object):
             return None
         if to and to not in all_vers:
             return None
-        cur_ver_idx = all_vers.index(cur_ver)
-        to_ver_idx = None
-        if not to:
-            to_ver_idx = len(all_vers)
+        # find out whether it's upgrade or downgrade
+        if is_upgrade(cur_ver, to):
+            cur_ver_idx = all_vers.index(cur_ver)
+            to_ver_idx = None
+            if not to:
+                to_ver_idx = len(all_vers)
+            else:
+                to_ver_idx = all_vers.index(to) + 1
+            return all_vers[cur_ver_idx+1:to_ver_idx]
         else:
-            to_ver_idx = all_vers.index(to)
-        return all_vers[cur_ver_idx+1:to_ver_idx]
+            all_vers.reverse()
+            cur_ver_idx = all_vers.index(cur_ver)
+            to_ver_idx = all_vers.index(to) + 1
+            return all_vers[cur_ver_idx+1:to_ver_idx]
