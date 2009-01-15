69d53b6c82a40720777d07f3df001ecc97159dbe
km <km@webdrive.ru>
2009-01-15 18:29:23 +0300

    optparse introduction in test.py
    test.py can be controlled via command line using optparse library.

--- test.py
+++ test.py
@@ -1,35 +1,81 @@
 #!/usr/bin/env python
 
+import os
 import sys
 
-from manager import Manager
-import worker
-import version_catalog
 
-conn_str = 'sqlite:///dbtest.sqlite'
+from optparse import OptionParser
 
-action = sys.argv[1].lower()
 
-worker = worker.SqlWorker(conn_str)
-catalog = version_catalog.PlainFilesVersionCatalog('example_catalogs/plain_files')
-# catalog = version_catalog.DirectoryVersionCatalog('example_catalogs/directories')
+from dbup.manager import Manager
+from dbup.worker import SqlWorker
+from dbup import version_catalog
 
-m = Manager(worker=worker, catalog=catalog)
 
-if action == 'up':
-    if len(sys.argv) <3:
-        m.upgrade(None)
-    else:
-        m.upgrade(sys.argv[2])
-elif action == 'dn':
-    m.downgrade(sys.argv[2])
-elif action == 'del':
-    m.uninstall()
-elif action == 'cur':
-    cur_ver = worker.get_current_version()
-    if cur_ver is not None:
-        print "Current version deployed: %s" % cur_ver
-    else:
-        print "No installation detected."
-else:
-    print "Unknown action."
+USAGE = """Usage: %prog ( up [ VER ] | down VER | delete | check )
+
+    up [VER]    upgrade DB to version VER
+                Ommit VER to upgrade to the latest available version.
+
+    down VER    downgrade DB to version VER
+                Downgrading to version 0 will leave you with that version, not before it.
+
+    delete      downgrade before first version
+                Will also destroy dbup internal information in DB.
+                In clean case you expect no tables in database after this operation.
+
+    check       show current version and exit"""
+
+VALID_ACTIONS = ['up', 'down', 'delete', 'status']
+
+
+def main(argv=sys.argv):
+    parser = OptionParser(usage=USAGE)
+    parser.add_option('-c', '--connection-string',
+                      default="",
+                      metavar="STRING",
+                      help="Connection string to database server.")
+    parser.add_option('-p', '--version-path',
+                      default="",
+                      metavar="PATH",
+                      help="Path to versions.")
+
+    options, arguments = parser.parse_args(argv)
+    if len(arguments) < 2 or arguments[1] not in VALID_ACTIONS:
+        parser.print_help()
+        exit(1)
+
+    if not all([options.connection_string, options.version_path]):
+        parser.print_help()
+        exit(1)
+
+    worker = SqlWorker(options.connection_string)
+    catalog = version_catalog.DirectoryVersionCatalog(options.version_path)
+    manager = Manager(worker=worker, catalog=catalog)
+
+    action = arguments[1]
+
+    if action == 'up':
+        ver = None
+        if len(arguments) > 2:
+            ver = arguments[2]
+        manager.upgrade(ver)
+    elif action == 'down':
+        if len(arguments) > 2:
+            ver = arguments[2]
+        else:
+            parser.print_help()
+            exit(1)
+        manager.downgrade(ver)
+    elif action == 'delete':
+        manager.uninstall()
+    elif action == 'status':
+        current_version = worker.get_current_version()
+        if current_version is not None:
+            print "Current version deployed: %s" % current_version
+        else:
+            print "No installation detected."
+
+
+if __name__ == '__main__':
+    main()
